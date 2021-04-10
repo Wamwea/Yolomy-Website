da10ed4518a4533a9066a5350d6c1f2b78f01235
wamwea <mynameiswamwea@gmail.com>
2021-04-10 23:04:34 +0300

    toggle functionality for product listings implemented

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ $(document).ready(function() {
       $('html, body, #hero').animate({ scrollTop: $('#scroll-to').offset().top}, 500, 'linear');
     });
   });
+ 
+$(document).ready(function(){
+    $('#men-checkbox').attr('checked', true).show();
+    $("#men-checkbox").on('click', function(){
+      $("#men-listing").toggle(700);
+    });});
+
+    $(document).ready(function(){
+        $('#women-checkbox').attr('checked', true).show();
+        $("#women-checkbox").on('click', function(){
+          $("#women-listing").toggle(700);
+        });});
+$(document).ready(function(){
+
+    $("#products-button").on('click',function(){
+        $("#men-listing,#women-listing").show(700);
+        
+        $('#men-checkbox, #women-checkbox').attr('checked', true).show();  
+    })
+})
